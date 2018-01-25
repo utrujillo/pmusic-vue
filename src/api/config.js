a6e2180994baf90456eb0b4f8f21d9e91d2d3239
Uziel Trujillo <uziel.trujillo@gmail.com>
2018-01-24 22:48:03 -0600

    [#154621855] Platzi music
    Nota:
    Se utiliza la API de Last FM para obtener informacion y probar Vue
    	new file:   .babelrc
    	new file:   .editorconfig
    	new file:   .gitignore
    	new file:   README.md
    	new file:   index.html
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   src/App.vue
    	new file:   src/api/config.js
    	new file:   src/api/index.js
    	new file:   src/assets/logo.png
    	new file:   src/components/Artist.vue
    	new file:   src/components/Spinner.vue
    	new file:   src/main.js
    	new file:   webpack.config.js

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appName: 'pmusic',
+  apiKey: 'f787be83c1d2d64dd496d0e4c323bc12',
+  secret: '917cd5773d5e4761b37b9b30dc11f71b',
+  registeredTo: 'uzotrujillo'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  li.artist 
+    h2: a(v-bind:href="artist.url" target="_blank") {{ artist.name }}
+    img(v-bind:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  name: 'artist', //este nombre tiene que coincidir con el nombre con el que se llama desde App.vue
+  props: [
+    'artist', // Es la propiedad que se pasa utilizando el v-bind
+  ]
+}
+</script>
+
+<style lang="stylus" scoped>
+  li.artist
+    display block
+    margin 0 10px
+</style>
